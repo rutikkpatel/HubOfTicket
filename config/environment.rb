3ac153dd9caa573d43b82e2f3c3d1abca6fc2a60
rutikkpatel <rutikkpatel@gmail.com>
2023-10-25 19:44:16 -0300

    Implement confirmation mail functionality

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'hubofticket@gmail.com',
+  password:             'fncp bpqo sjkp qxib',
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
